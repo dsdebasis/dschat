b18f3bf2f4cc630f342dca3d7fa71e5da367789b
Debasis Sahoo <dsdebasis2@gmail.com>
2024-09-20 23:13:09 +0530

    mongo uri updated

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	// console.log(process.env.MONGODB_URI)
+	const uri = process.env.MONGODB_URI
 	try {
-		const conn = await mongoose.connect( process.env.MONGODB_URI);
+		const conn = await mongoose.connect( uri);
 		console.log("Connected to MongoDB",conn.connection.host);
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
